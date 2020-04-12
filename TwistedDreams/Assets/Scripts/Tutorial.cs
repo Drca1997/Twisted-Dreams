870e8d7539da21472e93b2a0adc6d7fd21dc22d3
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2020-04-12 17:42:41 +0100

    GlowingObjects
    Na Cena Tutorial, Objectos so começam a piscar no fim do dialogo

---
+++ TwistedDreams/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    
+    private bool DialogFinished = false;
+    private bool dialogStarted = false;
+    
+
+    private void Update()
+    {
+
+        GlowingObjects();
+    }
+
+    public void GlowingObjects()
+    {
+        if (gameObject.GetComponentInChildren<DialogSystem>().active && !dialogStarted)
+        {
+            dialogStarted = true;
+        }
+        else if (!gameObject.GetComponentInChildren<DialogSystem>().active && dialogStarted && !DialogFinished)
+        {
+            DialogFinished = true;
+        }
+        else if (DialogFinished)
+        {
+
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<cakeslice.OutlineEffect>().enabled = true;
+            this.enabled = false;
+
+        }
+    }
+}
