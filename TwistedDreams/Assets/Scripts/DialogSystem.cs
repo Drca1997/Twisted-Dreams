5f294f9bd5d35c23668188adad2e023c6fd9664e
tgcunha999 <33125227+tgcunha999@users.noreply.github.com>
2020-04-15 11:23:48 +0100

    Revert "Update DialogSystem.cs"
    This reverts commit a13257def7b904baf4e64a7b84e5092a8677a5b9.

--- TwistedDreams/Assets/Scripts/DialogSystem.cs
+++ TwistedDreams/Assets/Scripts/DialogSystem.cs
@@ -1,32 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class DialogSystem : MonoBehaviour
 {
     public GameObject textBoxPanel;
     public GameObject whoBoxPanel;
 
-    public Text contText;
     public Text theText;
     public Text whoText;
 
-    private string cont = "Press space to continue...";
-    private string fin = "Press space to finish...";
-
     public TextAsset textFile;
     private string[] dialogLines;
-    private string currentText;
 
     private int currentLine;
     private int endAtLine;
-    private bool finished = true;
-    private bool finished_current_line = false;
 
     public bool active = false;
     public bool running = false;
-
-    public float typeDelay = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -47,35 +37,17 @@ public class DialogSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (active && finished)
+        if (active)
         {
             running = true;
             textBoxPanel.SetActive(true);
             whoBoxPanel.SetActive(true);
 
             whoText.text = dialogLines[currentLine - 1];
-
-            if (!finished_current_line)
-            {
-                contText.text = fin;
-                Coroutine st = StartCoroutine(showText(currentLine));
-            }
+            theText.text = dialogLines[currentLine];
 
             if (Input.GetKeyDown(KeyCode.Space))
-            {
-                if (finished_current_line) 
-                {
-                    currentLine += 2;
-                    finished_current_line = false; 
-                }
-                //else
-                //{
-                //    StopCoroutine(st);
-                //    theText.text = dialogLines[currentLine];
-                //    finished = true;
-                //    finished_current_line = true;
-                //}
-            }
+                currentLine += 2;
 
             if (currentLine > endAtLine)
             {
@@ -97,27 +69,5 @@ public class DialogSystem : MonoBehaviour
                 currentLine = 1;
             }
         }
-    }
-
-    IEnumerator showText(int lineNum)
-    {
-        finished = false;
-        for(int i=0; i < dialogLines[lineNum].Length; i++)
-        {
-            //if (Input.GetKeyDown(KeyCode.Space))
-            //{
-            //    theText.text = dialogLines[lineNum];
-            //    finished = true;
-            //    finished_current_line = true;
-            //    contText.text = cont;
-            //    yield break;
-            //}
-            currentText = dialogLines[lineNum].Substring(0, i);
-            theText.text = currentText;
-            yield return new WaitForSeconds(typeDelay);
-        }
-        finished = true;
-        finished_current_line = true;
-        contText.text = cont;
     }
 }
