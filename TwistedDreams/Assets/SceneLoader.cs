82ae653eb2a38b506761e32339cc5953113acc63
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2020-04-25 18:55:14 +0100

    Tutorial mais polido

---
+++ TwistedDreams/Assets/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private Scene cena;
+    private string next_scene_name;
+
+    private void Start()
+    {
+        cena = SceneManager.GetActiveScene();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            next_scene_name = Determina_Nova_Cena();
+            SceneManager.LoadScene(next_scene_name);
+        }
+    }
+
+    private string Determina_Nova_Cena()
+    {
+        string scene_name = null;
+        if (cena.name == "Tutorial")
+        {
+            scene_name = "Quente_Frio";
+        }
+        return scene_name;
+    }
+}
--- TwistedDreams/Assets/Scripts/Tutorial.cs
+++ TwistedDreams/Assets/Scripts/Tutorial.cs
@@ -4,26 +4,46 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
-    private int passedTime;
     [SerializeField]
-    private int startDialogTime = 500;
-    
+    private int startDialogTime;
+    [SerializeField]
+    private float startedWalkingTime;
+    private bool startedWalking;
+    [Tooltip("Time To Trigger Dialog If Started Walking (TTDISW)")]
+    [SerializeField]
+    private float tttdisw;
     public bool dialogStarted = false;
+    public TMPro.TextMeshPro text;
+    [SerializeField]
+    [Tooltip("Move-Text-to-Disappear-After-Started-Walking Time (MTDASWT)")]
+    private float mtdaswt;
+    public TMPro.TextMeshPro porta_prompt;
 
     private void Start()
     {
-        passedTime = 0;
+       
+        startedWalking = false;
     }
 
     private void Update()
     {
-        if (passedTime > startDialogTime)
+        if (Time.time > startDialogTime || (Time.time - startedWalkingTime > tttdisw && startedWalking))
         {
             gameObject.GetComponentInChildren<DialogSystem>().ActivateDialog();
+            if (text != null)
+            {
+                Destroy(text);
+            }
         }
-        else
+        if (!startedWalking && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
         {
-            passedTime++;
+            startedWalkingTime = Time.time;
+            startedWalking = true;
+            
+        }
+        if (startedWalking && Time.time - startedWalkingTime > mtdaswt)
+        {
+            Destroy(text);
         }
         if (gameObject.GetComponentInChildren<DialogSystem>().Is_Dialog_Finished()){
             GlowingObjects();
@@ -44,8 +64,14 @@ public class Tutorial : MonoBehaviour
     public void DoorAnimation(GameObject porta) {
         
         porta.GetComponentInChildren<Animator>().SetTrigger("Abrir");
+        porta.GetComponent<BoxCollider>().enabled = false;
+        porta.GetComponentInChildren<cakeslice.Outline>().enabled = false;
+        porta_prompt.SetText("");
+        
         
     }
+
+    
     
     
 }
