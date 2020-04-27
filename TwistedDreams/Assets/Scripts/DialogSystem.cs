3464c7754831a081ccd944dee01b836744870991
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2020-04-27 20:24:27 +0100

    u

--- TwistedDreams/Assets/Scripts/DialogSystem.cs
+++ TwistedDreams/Assets/Scripts/DialogSystem.cs
@@ -80,7 +80,7 @@ public class DialogSystem : MonoBehaviour
         contPanel.SetActive(true);
         writtingEffect = true;
         writefText.text = disabWrit;
-        movable = false;
+        movable = true;
 
         call = autoDialog;
 
