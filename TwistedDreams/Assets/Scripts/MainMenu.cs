8b47a1b79d7f1540e2743ad4d939f455ab099f5e
tgcunha999 <33125227+tgcunha999@users.noreply.github.com>
2020-05-06 12:25:32 +0100

    main menu v0.00000000001

---
+++ TwistedDreams/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject OptionsPanel;
+
+    public void PlayGame()
+    {
+        GameObject.Find("Canvas").SetActive(false);
+        StartCoroutine(Waiting());
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSecondsRealtime(Time.fixedDeltaTime * 5);
+    }
+
+    public void invertMenu()
+    {
+        MenuPanel.SetActive(!MenuPanel.activeSelf);
+        OptionsPanel.SetActive(!OptionsPanel.activeSelf);
+    }
+
+    public void changeAutoDialog(bool new_value)
+    {
+        PlayerPrefs.SetInt("AutoDialog",new_value ? 1 : 0);
+        // To retrieve
+        // autoDialog = PlayerPrefs.GetInt("Test") == 1 ? true : false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
