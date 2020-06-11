35a2bc71e0cc517a55f52ceb6a77ce07117c0f04
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2020-06-11 13:12:12 +0100

    JohnLevel

---
+++ TwistedDreams/Assets/Exiting_Time.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exiting_Time : MonoBehaviour
+{
+
+    public float time;
+    public GameObject C;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            C.GetComponent<Head_Animations>().Close_Eyes_Anim("MenuInicial");
+        }
+    }
+}
