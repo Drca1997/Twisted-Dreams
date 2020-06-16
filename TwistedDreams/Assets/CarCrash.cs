37f90dd11df50df7d29624196a966b377df6681f
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2020-06-17 00:18:43 +0100

    Update Car Crash Sound

---
+++ TwistedDreams/Assets/CarCrash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCrash : MonoBehaviour
+{
+    private Som crash_sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crash_sound = FindObjectOfType<AudioManager>().getSom("Car_Crash");
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        crash_sound.source.Play();   
+    }
+
+}
